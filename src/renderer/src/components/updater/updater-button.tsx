8906cca39c34238a390fa2da96cbe16fc3225bcf
Oranjekop <123@gmail.com>
2026-09-21 15:37:46 +0800

    fix: keep update button within sidebar header

--- src/renderer/src/components/updater/updater-button.tsx
+++ src/renderer/src/components/updater/updater-button.tsx
@@ -78,7 +78,7 @@ const UpdaterButton: React.FC<Props> = (props) => {
       ) : (
         <Button
           isIconOnly
-          className={`fixed right-11.25 app-nodrag`}
+          className="app-nodrag ml-auto mr-2 shrink-0"
           color="danger"
           size="sm"
           onPress={() => {
